c6824cf00df64acbc864036c5d387a138604894f
giacomo.alfani@student.univaq.it <giacomo.alfani@student.univaq.it>
2024-10-13 10:23:59 +0200

    renamed generated_graphs_validator.py and generated_graphs_creator.py in graph_creator and graph_validator. fixed also in comments and visuals

--- test/validation_test/graphs_creator.py
+++ test/validation_test/graphs_creator.py
@@ -1,7 +1,7 @@
 """
 This script is used to generate a fixed number of simple graphs using
 create_connected_simple_random_graph_with_weights function with fixed number of nodes and
-random number of edges, saving them in CSV files in test graphs/generated_graphs.
+random number of edges set in the main, saving them in CSV files in test graphs/generated_graphs.
 """
 
 import os
@@ -11,11 +11,12 @@ from graphs.nx_simple_connected_graph_generator import create_connected_simple_r
 
 def generate_and_save_graphs(nodes_list, output_dir):
 
-    # cretes the output directory if it doesn't exist
+    # creates the output directory if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
 
     for num_nodes in nodes_list:
         print(f"Generating graph with {num_nodes} nodes...")
+
         # graph generation
         graph = create_connected_simple_random_graph_with_weights(num_nodes, num_edges=None, fixed_weight=1)
 
@@ -26,22 +27,29 @@ def generate_and_save_graphs(nodes_list, output_dir):
         # save the graph to a CSV file
         output_file = os.path.join(output_dir, f"{num_nodes}_nodes.csv")
         df.to_csv(output_file, index=False, header=False)
+
         print(f"Saved graph with {num_nodes} nodes to {output_file}")
 
 
 if __name__ == '__main__':
 
     """ GRAPHS CREATION """
+    """ with the setting of the parameters below, it can be generated num_graphs graphs with num_nodes nodes each
+     and the number of edges are randomly selected. The step parameter sets the increment of the number of nodes."""
 
-    # fixed parameters
+    # fixed parameters, change this parameters if needed
     num_graphs = 20  # total number of graphs to generate
     start_nodes = 10  # starting number of nodes
     step = 10  # increment of number of nodes
 
     max_nodes = start_nodes + (num_graphs-1) * step
-    nodes_list = [i for i in range(start_nodes, max_nodes, step)]  # list of nodes
-    print(nodes_list)   # to see the list
+    nodes_list = [i for i in range(start_nodes, max_nodes, step)]
 
-    output_dir = 'test_graphs/generated_graphs'  # output directory
+    # to see the list of nodes
+    print(nodes_list)
 
-    generate_and_save_graphs(nodes_list, output_dir)    # generates and saves the graphs
+    # output directory
+    output_dir = 'test_graphs/generated_graphs'
+
+    # generates and saves the graphs
+    generate_and_save_graphs(nodes_list, output_dir)
